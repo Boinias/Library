5585ceac29b95771a745526651352ad2f2f26feb
Jack Francis <jack.francis1100@gmail.com>
2023-06-28 13:22:17 +0100

    Undefined inputs problem fixed

--- script.js
+++ script.js
@@ -59,89 +59,27 @@ function displayObjs () {
     })
 };
 
-
-// document.getElementById('form').addEventListener('submit', submitButtonTrigger)
-
-// function submitButtonTrigger(e) {
-//     e.preventDefault();
-//     var author = document.getElementById('author').value;
-//     var name = document.getElementById('name').value;
-//     var pages = document.getElementById('pages').value;
-//     var readStatus = document.getElementById('readStatus').checked;
-  
-//     var book = {
-//       author: author,
-//       title: name,
-//       pages: pages,
-//       readStatus: readStatus ? 'read' : 'unread'
-//     };
-  
-//     library.push(book);
-      
-//     console.log(library);
-    
-//     displayObjs();
-//     hideForm();
-//     clearForm();
-//   }
-
     form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    var author = document.getElementById('author').value;
-    var name = document.getElementById('name').value;
-    var pages = document.getElementById('pages').value;
-    var readStatus = document.getElementById('readStatus').checked;
-  
-    var book = {
-      author: author,
-      title: name,
-      pages: pages,
-      readStatus: readStatus ? 'read' : 'unread'
-    };
-  
-    library.push(book);
+        e.preventDefault();
       
-    console.log(library);
-    
-    displayObjs();
-    hideForm();
-    clearForm();
-    });
-
-
-
-// function displayObj () {
-//     var newObj = document.createElement('div')
-//     var library = document.getElementById('library');
-
-//     newObj.style.width = "300px";
-//     newObj.style.height = '100px';
-//     newObj.style.backgroundColor = 'green';
-
-//     for (i=0; i < 5; i++) {
-//     var newDetail = document.createElement('div')
-
-//     newdetail.style.width = "80px";
-//     newdetail.style.height = '80px';
-//     newdetail.style.backgroundColor = 'white';
-//     newdetail.style.color = 'black'
-//     newdetail.innerText = `${library[library.length-1].title}`;
-
-//     newObj.appendChild(newDetail);
-//     }
-
-//     function findReadStatus {
-
-//          }
-         
-//     }
-//     var changeReadStatus = document.createElement('button')
-//     changeReadStatus.textContent = readStatus of this book
-//     newObj.appendChild(changeReadStatus);
-
-//     var del = document.createElement('button')
-//     newObj.appendChild(del);
-
-//     library.appendChild(newObj);
-// };
+        var authorForBook = document.getElementById('author').value;
+        var titleForBook = document.getElementById('name').value;
+        var pagesForBook = document.getElementById('pages').value;
+        var readStatusForBook = document.getElementById('readStatus').checked;
+        
+        var book = {
+          author: authorForBook,
+          title: titleForBook,
+          pages: pagesForBook,
+          readStatus: readStatusForBook ? 'read' : 'unread'
+        };
+        
+        library.push(book);
+            
+        console.log(library);
+        
+        displayObjs();
+        hideForm();
+        clearForm();
+        
+      });
